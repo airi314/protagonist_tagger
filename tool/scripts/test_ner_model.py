81e09dd1cdae8f0968a18856b67ea1802e61cb04
airi314 <olka.wichrowska@gmail.com>
2021-11-11 20:10:28 +0100

    refactor test_ner_model

--- tool/scripts/test_ner_model.py
+++ tool/scripts/test_ner_model.py
@@ -1,23 +1,11 @@
 import argparse
-import re
 import os
 import json
 
-from tool.file_and_directory_management import read_file_to_list, read_sentences_from_file
-from tool.data_generator import json_to_spacy_train_data, spacy_format_to_json
-from tool.file_and_directory_management import dir_path, file_path
+from tool.file_and_directory_management import read_file_to_list, read_sentences_from_file, dir_path, file_path
 from tool.model.utils import load_model
 
-# titles_path - path to .txt file with titles of novels from which the sampled data are to be generated (titles should
-#       not contain any special characters and spaces should be replaced with "_", for example "Pride_andPrejudice")
-# names_gold_standard_dir_path - path to directory with .txt files containing gold standard with annotations being full
-#       names of literary characters (names of files should be the same as corresponding novels titles on the titles
-#       list)
-# generated_data_dir - directory where generated data should be stored
-# testing_data_dir_path - directory containing .txt files with sentences extracted from novels to be included in the
-#       testing process
-# ner_model_dir_path - path to directory containing fine-tune NER model to be tested; if None standard spacy NER
-#       model is used
+
 def main(titles_path, testing_data_dir_path, generated_data_dir, library='spacy', ner_model=None):
     titles = read_file_to_list(titles_path)
 
@@ -38,11 +26,17 @@ def main(titles_path, testing_data_dir_path, generated_data_dir, library='spacy'
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('titles_path', type=file_path)
-    parser.add_argument('testing_data_dir_path', type=dir_path)
-    parser.add_argument('generated_data_dir', type=str)
-    parser.add_argument('library', type=str, default='spacy', nargs='?')
-    parser.add_argument('ner_model', type=str, default=None, nargs='?')
+    parser.add_argument('titles_path', type=file_path,
+                        description="path to .txt file with titles of novels for which NER model should be tested")
+    parser.add_argument('testing_data_dir_path', type=dir_path,
+                        description="path to the directory containing .txt files with sentences extracted from novels "
+                                    "to be included in the testing process")
+    parser.add_argument('generated_data_dir', type=str,
+                        description="directory where generated data should be stored")
+    parser.add_argument('library', type=str, default='spacy', nargs='?',
+                        description="library which should be used to test NER")
+    parser.add_argument('ner_model', type=str, default=None, nargs='?',
+                        description="model from chosen library which should be used to test NER")
     opt = parser.parse_args()
     main(opt.titles_path, opt.testing_data_dir_path,
          opt.generated_data_dir, opt.library, opt.ner_model)
