d271b4f28850c787840dab2fb1a007163e4bb337
airi314 <olka.wichrowska@gmail.com>
2022-02-21 14:38:51 +0100

    fixes in evaluation scripts

--- tool/evaluation/get_ner_metrics.py
+++ tool/evaluation/get_ner_metrics.py
@@ -10,8 +10,6 @@ if __name__ == "__main__":
     parser.add_argument('results_dir', type=str)
     parser.add_argument('stats_path', type=str,
                         help="path to .txt file with titles of novels for which NER model should be tested")
-    parser.add_argument('--protagonist_tagger', action='store_true',
-                        help="if metrics for protagonist_tagger should be printed")
     parser.add_argument('--save_path', type=str,
                         help="if True then results will be saved in chosen path")
     opt = parser.parse_args()
@@ -19,14 +17,9 @@ if __name__ == "__main__":
     headers = ['Model', 'Precision', 'Recall', 'F-measure']
     metrics_table = []
 
-    if opt.protagonist_tagger:
-        task = 'protagonist'
-    else:
-        task = 'ner'
-
-    for library in sorted(os.listdir(os.path.join('experiments', opt.results_dir, task))):
+    for library in sorted(os.listdir(os.path.join('experiments', opt.results_dir))):
         results = load_from_pickle(os.path.join('experiments', opt.results_dir,
-                                                task, library, opt.stats_path, 'overall_metrics'))
+                                                library, opt.stats_path, 'overall_metrics'))
         library = library.replace('__', ' ')
         metrics_table.append([library] + results[:3])
     print(tabulate.tabulate(metrics_table, headers=headers, tablefmt='latex_booktabs'))
